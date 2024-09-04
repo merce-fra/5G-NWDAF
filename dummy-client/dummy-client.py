7e7ddf1482cd5f4cda35848e5b46bc9b0f6f0a81
Vincent Artur <v.artur@fr.merce.mee.com>
2024-09-04 09:53:12 +0200

    Modified dummy client to accept all types of notifications

--- dummy-client/dummy-client.py
+++ dummy-client/dummy-client.py
@@ -1,16 +1,17 @@
 import logging
 
 import uvicorn
-from fastapi import FastAPI
-from nwdaf_api.models.nnwdaf_events_subscription_notification import NnwdafEventsSubscriptionNotification
+from fastapi import FastAPI, Request
+
 from starlette import status
 
 app = FastAPI()
 
 
 @app.post("/analytics-notification", status_code=status.HTTP_204_NO_CONTENT)
-async def analytic_notif(notif: NnwdafEventsSubscriptionNotification) :
-    logging.info(f"Received an analytics notification: {notif.model_dump_json(exclude_unset=True)}")
+async def analytic_notif(request: Request):
+    json_data = await request.json()
+    logging.info(f"Received an analytics notification: {json_data}")
     return
 
 
