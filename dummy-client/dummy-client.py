1d6d9c74c6045c17bd226b60da7ef3cb31fcf61f
Vincent Artur <v.artur@fr.merce.mee.com>
2024-09-04 15:32:18 +0200

    Fixed analytics notification after model update

--- dummy-client/dummy-client.py
+++ dummy-client/dummy-client.py
@@ -1,20 +1,19 @@
 import logging
 
 import uvicorn
-from fastapi import FastAPI, Request
-
+from fastapi import FastAPI
+from nwdaf_api.models.nnwdaf_events_subscription_notification import NnwdafEventsSubscriptionNotification
 from starlette import status
 
 app = FastAPI()
 
 
 @app.post("/analytics-notification", status_code=status.HTTP_204_NO_CONTENT)
-async def analytic_notif(request: Request):
-    json_data = await request.json()
-    logging.info(f"Received an analytics notification: {json_data}")
+async def analytic_notif(notif: NnwdafEventsSubscriptionNotification):
+    logging.info(f"Received an analytics notification: {notif.model_dump_json(exclude_unset=True)}")
     return
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
     uvicorn.run(app, host='0.0.0.0', port=8181, log_level='warning', loop='asyncio')
