1314069879e3852d0c6ae45dcf77a7ebd4440932
Vincent Artur <v.artur@fr.merce.mee.com>
2024-11-04 16:23:13 +0100

    Added documentation

--- services/throughput-anlf/README.md
+++ services/throughput-anlf/README.md
@@ -1,14 +1,43 @@
 # Throughput AnLF
 
+## Overview
+
+The `ThroughputAnlfService` is a service designed to handle _UE_ throughput analytics in an _NWDAF_ microservice-based
+architecture. It uses an [_LSTM_ model](https://merce-gitlab.fr-merce.mee.com/gitlab/merckube/merckube-lstm) to predict
+a throughput value based on data collected from the _[GMLC](https://en.wikipedia.org/wiki/GMLC)_ and the
+_[RAN](https://en.wikipedia.org/wiki/Radio_access_network)_.
+
+## Pre-requisites
+
+To use the `ThroughputAnlfService`, you need to have the following pre-requisites:
+
+* _Python_ ≥ 3.12 (preferably in a _virtualenv_, or using [Conda](https://anaconda.org/anaconda/conda))
+
+You can then install the required third-party library with this command:
+
+```bash
+pip install -r requirements.txt
+````
+
+# How does it work?
+
+Basic _AnLF_ operations (analytics subscription CRUD operations, event exposure mechanics, analytics delivery, _ML_
+model provisioning, etc.) are
+inherited from
+the [AnlfService](https://merce-gitlab.fr-merce.mee.com/gitlab/artur/nwdaf-libcommon/-/blob/main/src/nwdaf_libcommon/AnlfService.py)
+class from the [nwdaf-libcommon](https://merce-gitlab.fr-merce.mee.com/gitlab/artur/nwdaf-libcommon) library.
+
+Finite-state machines are used to handle subscriptions concurrently. Each subscription has its own [FSM](https://merce-gitlab.fr-merce.mee.com/gitlab/artur/nwdaf-libcommon/-/blob/main/src/nwdaf_libcommon/FiniteStateMachine.py) with the
+following states and transitions:
 
 ```mermaid
 stateDiagram
     [*] --> INITIALIZING
-    INITIALIZING --> WAITING_FOR_GMLC_NOTIF : INITIALIZATION_DONE
-    WAITING_FOR_GMLC_NOTIF --> PREDICTING_THROUGHPUT : ALL_NOTIFS_RECEIVED
-    WAITING_FOR_GMLC_NOTIF --> WAITING_FOR_RAN_NOTIF : WAITING_FOR_NOTIFS
-    WAITING_FOR_RAN_NOTIF --> PREDICTING_THROUGHPUT : ALL_NOTIFS_RECEIVED
-    WAITING_FOR_RAN_NOTIF --> WAITING_FOR_GMLC_NOTIF : WAITING_FOR_NOTIFS
-    PREDICTING_THROUGHPUT --> SENDING_ANALYTICS_NOTIF : PREDICTION_DONE
-    SENDING_ANALYTICS_NOTIF --> WAITING_FOR_GMLC_NOTIF : ANALYTICS_NOTIF_SENT
+    INITIALIZING --> WAITING_FOR_GMLC_NOTIF: INITIALIZATION_DONE
+    WAITING_FOR_GMLC_NOTIF --> PREDICTING_THROUGHPUT: ALL_NOTIFS_RECEIVED
+    WAITING_FOR_GMLC_NOTIF --> WAITING_FOR_RAN_NOTIF: WAITING_FOR_NOTIFS
+    WAITING_FOR_RAN_NOTIF --> PREDICTING_THROUGHPUT: ALL_NOTIFS_RECEIVED
+    WAITING_FOR_RAN_NOTIF --> WAITING_FOR_GMLC_NOTIF: WAITING_FOR_NOTIFS
+    PREDICTING_THROUGHPUT --> SENDING_ANALYTICS_NOTIF: PREDICTION_DONE
+    SENDING_ANALYTICS_NOTIF --> WAITING_FOR_GMLC_NOTIF: ANALYTICS_NOTIF_SENT
 ```
--- services/throughput-anlf/ThroughputSubscriptionFSM.py
+++ services/throughput-anlf/ThroughputSubscriptionFSM.py
@@ -1,9 +1,18 @@
 from enum import StrEnum
-
 from nwdaf_libcommon.FiniteStateMachine import FiniteStateMachine
 
 
 class States(StrEnum):
+    """
+    Enumeration of the states for the throughput subscription finite state machine (FSM).
+
+    Attributes:
+        INITIALIZING: The initial state where the subscription is being set up.
+        WAITING_FOR_GMLC_NOTIF: The state where the FSM is waiting for notifications from GMLC.
+        WAITING_FOR_RAN_NOTIF: The state where the FSM is waiting for notifications from RAN.
+        PREDICTING_THROUGHPUT: The state where the FSM is performing throughput prediction.
+        SENDING_ANALYTICS_NOTIF: The state where the FSM is sending analytics notifications.
+    """
     INITIALIZING = "INITIALIZING",
     WAITING_FOR_GMLC_NOTIF = "WAITING_FOR_GMLC_NOTIF",
     WAITING_FOR_RAN_NOTIF = "WAITING_FOR_RAN_NOTIF",
@@ -12,6 +21,16 @@ class States(StrEnum):
 
 
 class Transitions(StrEnum):
+    """
+    Enumeration of the transitions for the throughput subscription finite state machine (FSM).
+
+    Attributes:
+        INITIALIZATION_DONE: Transition indicating that initialization is complete.
+        ALL_NOTIFS_RECEIVED: Transition indicating that all required notifications have been received.
+        WAITING_FOR_NOTIFS: Transition indicating that the FSM is waiting for additional notifications.
+        PREDICTION_DONE: Transition indicating that throughput prediction is complete.
+        ANALYTICS_NOTIF_SENT: Transition indicating that the analytics notification has been sent.
+    """
     INITIALIZATION_DONE = "INITIALIZATION_DONE",
     ALL_NOTIFS_RECEIVED = "ALL_NOTIFS_RECEIVED",
     WAITING_FOR_NOTIFS = "WAITING_FOR_NOTIFS",
@@ -20,6 +39,28 @@ class Transitions(StrEnum):
 
 
 class ThroughputSubscriptionFSM(FiniteStateMachine):
+    """
+    Finite State Machine (FSM) for managing throughput subscription states and transitions.
+
+    This FSM coordinates the states of a throughput subscription process, handling initialization,
+    waiting for notifications, predicting throughput, and sending analytics notifications.
+
+    Inherits from the FiniteStateMachine class, initializing with the defined states and transitions.
+
+    States:
+        - States.INITIALIZING: The FSM starts in this state while initializing the subscription.
+        - States.WAITING_FOR_GMLC_NOTIF: The FSM transitions to this state after initialization, waiting for GMLC notifications.
+        - States.WAITING_FOR_RAN_NOTIF: This state is entered when the FSM is waiting for RAN notifications.
+        - States.PREDICTING_THROUGHPUT: In this state, the FSM performs throughput predictions.
+        - States.SENDING_ANALYTICS_NOTIF: The FSM enters this state to send analytics notifications to GMLC.
+
+    Transitions:
+        - Transitions.INITIALIZATION_DONE: Triggers the transition from INITIALIZING to WAITING_FOR_GMLC_NOTIF.
+        - Transitions.ALL_NOTIFS_RECEIVED: Moves the FSM to the predicting state after all notifications are received.
+        - Transitions.WAITING_FOR_NOTIFS: Allows the FSM to wait for additional notifications if needed.
+        - Transitions.PREDICTION_DONE: Transitions the FSM to the sending notifications state once the prediction is done.
+        - Transitions.ANALYTICS_NOTIF_SENT: Returns the FSM to the WAITING_FOR_GMLC_NOTIF state after sending analytics notifications.
+    """
 
     def __init__(self):
         super().__init__(
